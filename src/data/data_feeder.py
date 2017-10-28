9b2b593d612497db6c93b02e071f9db59302d533
Piotr Oleksy <piotr.olek@o2.pl>
2017-10-28 13:31:44 +0200

    minor refactor

--- src/data/data_feeder.py
+++ src/data/data_feeder.py
@@ -10,51 +10,31 @@ class DataFeeder(object):
         """
         self.patient_list = patient_list
 
+    # defaults
+
     def get_number_of_patients(self):
         return len(self.patient_list)
-
-    def get_initial_weight_list(self):
-        return [patient.weight_initially for patient in self.patient_list]
 
     def get_mean_initial_weight(self):
         return np.mean(self.get_initial_weight_list())
 
-    def get_weight_at_labour_list(self):
-        return [patient.weight_at_labour for patient in self.patient_list]
-
-    def get_number_of_miscarriages_list(self):
-        return [patient.number_of_miscarriages for patient in self.patient_list]
-
     def get_mean_weight_at_labour(self):
         return np.mean(self.get_weight_at_labour_list())
-
-    def get_child_weight_list(self):
-        return [patient.child_weight for patient in self.patient_list]
-
-    def get_child_weight_standard_deviation_list(self):
-        stds = []
-        for x in xrange(10, 50, 1):
-            list_filtered = filter(lambda patient: x < patient.BMI_initially <= x + 10, self.patient_list)
-            stds.append(np.std([patient.child_weight for patient in list_filtered]))
-
-        return stds
-
-    def get_child_weight_mean_list(self):
-        stds = []
-        for x in xrange(10, 50, 1):
-            list_filtered = filter(lambda patient: x < patient.BMI_initially <= x + 10, self.patient_list)
-            if len([patient.child_weight for patient in list_filtered]) != 0:
-                stds.append(np.mean([patient.child_weight for patient in list_filtered]))
-            else:
-                stds.append(0)
-
-        return stds
 
     def get_child_weight_standard_deviation(self):
         return np.std([patient.child_weight for patient in self.patient_list])
 
     def get_child_weight_mean(self):
         return np.mean([patient.child_weight for patient in self.patient_list])
+
+    def get_mean_initial_BMI(self):
+        return np.mean(self.get_initial_BMI_list())
+
+    def get_mean_thrombosis_risk(self):
+        return np.mean(self.get_thrombosis_risk_list())
+
+    def get_mean_length_of_pregnancy(self):
+        return np.mean(self.get_length_of_pregnancy_list())
 
     def get_percentage_of_first_timers(self):
         def is_first_timer(patient):
@@ -66,6 +46,37 @@ class DataFeeder(object):
         except ZeroDivisionError:
             return -1
 
+    # lists
+
+    def get_initial_weight_list(self):
+        return [patient.weight_initially for patient in self.patient_list]
+
+    def get_weight_at_labour_list(self):
+        return [patient.weight_at_labour for patient in self.patient_list]
+
+    def get_number_of_miscarriages_list(self):
+        return [patient.number_of_miscarriages for patient in self.patient_list]
+
+    def get_child_weight_list(self):
+        return [patient.child_weight for patient in self.patient_list]
+
+    def get_child_weight_standard_deviation_list(self):
+        stds = []
+        for x in xrange(10, 50, 1):
+            list_filtered = filter(lambda patient: x < patient.BMI_initially <= x + 10, self.patient_list)
+            stds.append(np.std([patient.child_weight for patient in list_filtered]))
+        return stds
+
+    def get_child_weight_mean_list(self):
+        stds = []
+        for x in xrange(10, 50, 1):
+            list_filtered = filter(lambda patient: x < patient.BMI_initially <= x + 10, self.patient_list)
+            if len([patient.child_weight for patient in list_filtered]) != 0:
+                stds.append(np.mean([patient.child_weight for patient in list_filtered]))
+            else:
+                stds.append(0)
+        return stds
+
     def get_delta_weight_list(self):
         return [patient.delta_weight for patient in self.patient_list]
 
@@ -75,27 +86,14 @@ class DataFeeder(object):
     def get_initial_BMI_list(self):
         return [patient.BMI_initially for patient in self.patient_list]
 
-    def get_mean_initial_BMI(self):
-        return np.mean(self.get_initial_BMI_list())
-
     def get_length_of_pregnancy_list(self):
         return [patient.pregnancy_time for patient in self.patient_list]
-
-    def get_mean_length_of_pregnancy(self):
-        return np.mean(self.get_length_of_pregnancy_list())
 
     def get_delta_weight_to_initial_weight_ratio_list(self):
         return [patient.delta_weight / patient.weight_initially for patient in self.patient_list]
 
     def get_count_of_early_births(self):
-        def is_early(patient):
-            if patient.pregnancy_time < 259:
-                return 1
-            return 0
-        return sum([is_early(patient) for patient in self.patient_list])
+        return sum([patient.preterm_birth for patient in self.patient_list])
 
     def get_thrombosis_risk_list(self):
         return [patient.thrombosis_risk for patient in self.patient_list]
-
-    def get_mean_thrombosis_risk(self):
-        return np.mean(self.get_thrombosis_risk_list())
--- src/statistics/runner.py
+++ src/statistics/runner.py
@@ -2,6 +2,27 @@ from src.data.data_feeder import DataFeeder
 from src.visualization.plotter import simple_plot, plot_and_save, plot_and_save_multiple
 
 
+def generate_feeder(patient_list, patient_arg, filter_, groups):
+    return [DataFeeder([patient for patient in patient_list if filter_(patient_arg(patient), group)])
+            for group in groups]
+
+
+def run_multiple_plotter(name, feeders, method1, method2):
+    args, vals = [], []
+    for feed in feeders:
+        arg, val = method1(feed), method2(feed)
+        args.append(arg)
+        vals.append(val)
+    plot_and_save_multiple(args, vals, name)
+
+
+def calculate_in_groups(name, feeders, method):
+    print name
+    for feed in feeders:
+        print method(feed)
+    print
+
+
 def run_statistics(patient_list):
     feeder = DataFeeder(patient_list)
 
@@ -15,113 +36,48 @@ def run_statistics(patient_list):
 
     ##### GROUPS
 
-    feeder_1 = DataFeeder([patient for patient in patient_list if patient.BMI_initially < 18.5])
-    feeder_2 = DataFeeder([patient for patient in patient_list if 18.5 <= patient.BMI_initially < 25])
-    feeder_3 = DataFeeder([patient for patient in patient_list if 25 <= patient.BMI_initially < 30])
-    feeder_4 = DataFeeder([patient for patient in patient_list if 30 <= patient.BMI_initially])
+    intervals = [(0, 18.5), (18.5, 25), (25, 30), (30, 100)]
+    method = lambda x, y: y[0] <= x < y[1]
 
-    feeders = [feeder_1, feeder_2, feeder_3, feeder_4]
+    feeders = generate_feeder(patient_list, lambda x: x.BMI_initially, method, intervals)
 
+    run_multiple_plotter('weight_change_percentage_to_child_weight_GROUPS',
+                         feeders,
+                         lambda x: x.get_weight_change_percentage_list(),
+                         lambda x: x.get_child_weight_list())
 
-    name = 'weight_change_percentage_to_child_weight_GROUPS'
-    args, vals = [], []
-    for feed in feeders:
-        arg, val = feed.get_weight_change_percentage_list(), feed.get_child_weight_list()
-        args.append(arg)
-        vals.append(val)
+    run_multiple_plotter('delta_weight_to_child_weight_GROUPS',
+                         feeders,
+                         lambda x: x.get_delta_weight_list(),
+                         lambda x: x.get_child_weight_list())
 
-    plot_and_save_multiple(args, vals, name)
+    run_multiple_plotter('BMI_to_mean_time_of_pregnancy_in_GROUPS_BMI_INITIALLY',
+                         feeders,
+                         lambda x: x.get_initial_BMI_list(),
+                         lambda x: x.get_length_of_pregnancy_list())
 
-    name = 'delta_weight_to_child_weight_GROUPS'
-    args, vals = [], []
-    for feed in feeders:
-        arg, val = feed.get_delta_weight_list(), feed.get_child_weight_list()
-        args.append(arg)
-        vals.append(val)
+    run_multiple_plotter('BMI_to_thrombosis_in_GROUPS_BMI_INITIALLY',
+                         feeders,
+                         lambda x: x.get_initial_BMI_list(),
+                         lambda x: x.get_thrombosis_risk_list())
 
-    plot_and_save_multiple(args, vals, name)
-
-    name = 'BMI_to_mean_time_of_pregnancy_in_GROUPS_BMI_INITIALLY'
-    args, vals = [], []
-    for feed in feeders:
-        arg, val = feed.get_initial_BMI_list(), feed.get_length_of_pregnancy_list()
-        args.append(arg)
-        vals.append(val)
-
-    plot_and_save_multiple(args, vals, name)
-
-    name = 'BMI_to_thrombosis_in_GROUPS_BMI_INITIALLY'
-    args, vals = [], []
-    for feed in feeders:
-        arg, val = feed.get_initial_BMI_list(), feed.get_thrombosis_risk_list()
-        args.append(arg)
-        vals.append(val)
-
-    plot_and_save_multiple(args, vals, name)
-
-    name = 'standard_deviation_in_GROUPS'
-    print name
-    for feed in feeders:
-        print feed.get_child_weight_standard_deviation()
-
-    name = 'mean_in_GROUPS'
-    print
-    print name
-    for feed in feeders:
-        print feed.get_child_weight_mean()
-
-    name = 'percentage_of_first_timers_in_GROUPS'
-    print
-    print name
-    for feed in feeders:
-        print feed.get_percentage_of_first_timers()
-
-    name = 'number_of_patients_in_GROUPS'
-    print
-    print name
-    for feed in feeders:
-        print feed.get_number_of_patients()
-
-    name = 'mean_time_of_pregnancy_in_GROUPS_BMI_INITIALLY'
-    print
-    print name
-    for feed in feeders:
-        print feed.get_mean_length_of_pregnancy()
-
-
-    name = 'count_of_early_births_in_GROUPS_BMI_INITIALLY'
-    print
-    print name
-    for feed in feeders:
-        print feed.get_count_of_early_births(), 'z', feed.get_number_of_patients(), '->', 1.*feed.get_count_of_early_births()/feed.get_number_of_patients(), '%'
-
-    name = 'mean_thrombosis_in_GROUPS_BMI_INITIALLY'
-    print
-    print name
-    for feed in feeders:
-        print feed.get_mean_thrombosis_risk()
+    calculate_in_groups('standard_deviation_in_GROUPS', feeders, lambda x: x.get_child_weight_standard_deviation())
+    calculate_in_groups('mean_in_GROUPS', feeders, lambda x: x.get_child_weight_mean())
+    calculate_in_groups('percentage_of_first_timers_in_GROUPS', feeders, lambda x: x.get_percentage_of_first_timers())
+    calculate_in_groups('number_of_patients_in_GROUPS', feeders, lambda x: x.get_number_of_patients())
+    calculate_in_groups('mean_time_of_pregnancy_in_GROUPS_BMI_INITIALLY', feeders, lambda x: x.get_mean_length_of_pregnancy())
+    calculate_in_groups('count_of_early_births_in_GROUPS_BMI_INITIALLY', feeders, lambda x: (x.get_count_of_early_births(), 'z', x.get_number_of_patients(), '->', 1.*x.get_count_of_early_births()/x.get_number_of_patients(), '%'))
+    calculate_in_groups('mean_thrombosis_in_GROUPS_BMI_INITIALLY', feeders, lambda x: x.get_mean_thrombosis_risk())
 
     ######### GROUPS 2
 
-    feeder_1 = DataFeeder([patient for patient in patient_list if patient.BMI_at_labour < 18.5])
-    feeder_2 = DataFeeder([patient for patient in patient_list if 18.5 <= patient.BMI_at_labour < 25])
-    feeder_3 = DataFeeder([patient for patient in patient_list if 25 <= patient.BMI_at_labour < 30])
-    feeder_4 = DataFeeder([patient for patient in patient_list if 30 <= patient.BMI_at_labour])
+    intervals = [(0, 18.5), (18.5, 25), (25, 30), (30, 100)]
+    method = lambda x, y: y[0] <= x < y[1]
 
-    feeders = [feeder_1, feeder_2, feeder_3, feeder_4]
+    feeders = generate_feeder(patient_list, lambda x: x.BMI_at_labour, method, intervals)
 
-    name = 'percentage_of_first_timers_in_GROUPS_BMI_AT_LABOUR'
-    print
-    print name
-    for feed in feeders:
-        print feed.get_percentage_of_first_timers()
-
-    name = 'number_of_patients_in_GROUPS_BMI_AT_LABOUR'
-    print
-    print name
-    for feed in feeders:
-        print feed.get_number_of_patients()
-
+    calculate_in_groups('percentage_of_first_timers_in_GROUPS_BMI_AT_LABOUR', feeders, lambda x: x.get_percentage_of_first_timers())
+    calculate_in_groups('number_of_patients_in_GROUPS_BMI_AT_LABOUR', feeders, lambda x: x.get_number_of_patients())
 
     ######### GROUPS 3
 
@@ -130,17 +86,8 @@ def run_statistics(patient_list):
 
     feeders = [feeder_1, feeder_2]
 
-    name = 'mean_patient_weight_initially_in_GROUPS_BY_NUMBER_OF_LABOURS'
-    print
-    print name
-    for feed in feeders:
-        print feed.get_mean_initial_weight()
-
-    name = 'mean_patient_weight_at_labour_in_GROUPS_BY_NUMBER_OF_LABOURS'
-    print
-    print name
-    for feed in feeders:
-        print feed.get_mean_weight_at_labour()
+    calculate_in_groups('mean_patient_weight_initially_in_GROUPS_BY_NUMBER_OF_LABOURS', feeders, lambda x: x.get_mean_initial_weight())
+    calculate_in_groups('mean_patient_weight_at_labour_in_GROUPS_BY_NUMBER_OF_LABOURS', feeders, lambda x: x.get_mean_weight_at_labour())
 
     ######### GROUPS 4
 
@@ -152,15 +99,6 @@ def run_statistics(patient_list):
 
     feeders = [feeder_1, feeder_2, feeder_3, feeder_4, feeder_5]
 
-    name = 'mean_patient_initial_BMI_in_GROUPS_BY_NUMBER_OF_MISCARRIAGES'
-    print
-    print name
-    for feed in feeders:
-        print feed.get_mean_initial_BMI()
-
-    name = 'number_of_patients_in_GROUPS_BY_NUMBER_OF_MISCARRIAGES'
-    print
-    print name
-    for feed in feeders:
-        print feed.get_number_of_patients()
+    calculate_in_groups('mean_patient_initial_BMI_in_GROUPS_BY_NUMBER_OF_MISCARRIAGES', feeders, lambda x: x.get_mean_initial_BMI())
+    calculate_in_groups('number_of_patients_in_GROUPS_BY_NUMBER_OF_MISCARRIAGES', feeders, lambda x: x.get_number_of_patients())
 
